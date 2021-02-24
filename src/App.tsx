9fb03c584b43ac48ed369283a39721674a24fc3e
Denis <denis@epics.com.br>
2021-02-23 22:17:43 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ExperienceBar } from './molecules/ExperienceBar'
+import './style/main.css'
+
+function App() {
+	return (
+		<div className="container">
+			<ExperienceBar/>						
+		</div>
+	);
+}
+
+export default App;
